1928384ad28aaa96b4e2cb8500a3fadd363fb6c7
amar2468 <plakaloam@gmail.com>
2025-06-14 17:43:30 +0100

    Added asteroid API specific routes, to get NEO data

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const express = require('express');
 // Import CORS middleware to allow cross-origin requests (from React to backend)
 const cors = require('cors');
 
+const asteroidRoutes = require('./routes/asteroids');
+
 // Load environment variables from .env file
 require('dotenv').config();
 
@@ -13,10 +15,12 @@ const app = express();
 // Enable CORS so frontend (on a different port) can access the backend
 app.use(cors());
 
+app.use('/api/asteroids', asteroidRoutes);
+
 // Root URL, with a basic message.
 app.get('/', (req, res) => {
     res.send('Backend is running');
-}); 
+});
 
 // Start the server and listen on port 4000
 app.listen(4000, () => {
